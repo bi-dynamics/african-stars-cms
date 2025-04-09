f6ae7261fa235cec83bda065a7a6b1254ce899f6
Luke Cloete <Cloeteluke@gmail.com>
2025-04-09 19:37:49 +0200

    Update config

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app =  initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const storage = getStorage(app);
